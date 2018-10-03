f5111dbd9a244cc022854dfea71931952284eff6
DanielrR0m0 <drdanielromo@gmail.com>
2018-10-03 09:56:17 -0700

    working on quiz

--- labs/quiz5/child.cpp
+++ labs/quiz5/child.cpp
@@ -2,18 +2,20 @@
 #include "child.h"
 using namespace std;
 
+Child::Child(){};
 
 void Child::recite(){
     cout << "I do not like green eggs and ham. \n"<<
             "I will not eat them, Sam I Am,"<<endl;
         
-        for(int i = 0; i < strings.size; i++)
+        for(int i = 0; i < (int) strings.size(); i++)
         {
-            cout<<strings[i]<<endl;
+            cout<<Child::strings[i]<<endl;
         }
         
         
 };
+
 void Child::askPlace(){
     string input;
     getline(cin,input);
--- labs/quiz5/child.h
+++ labs/quiz5/child.h
@@ -13,6 +13,6 @@ class Child{
     private:
         vector<string> strings;
 
-}
+};
 
 #endif
