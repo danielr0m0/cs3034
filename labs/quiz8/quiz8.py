4ded799c89099a949bc46cd3de900b4aeab54866
DanielrR0m0 <drdanielromo@gmail.com>
2018-11-15 09:26:37 -0800

    finish quizes

--- labs/quiz8/quiz8.py
+++ labs/quiz8/quiz8.py
@@ -7,16 +7,22 @@ def validate(min,max, msg):
             if(uMin<min or uMax>max):
                 raise ValueError()
             else:
-                f= []
+                f= {}
                 for i in range(uMin,uMax+1):
-                    f.append(convert(i))
-                print(f)
+                    f[i]=(convert(i))
+                print(int(len(f)**.5)+1)
+                printF(f)
                 error=False
         except ValueError:
             print("invalid input. please enter the "+ msg+ ", from the rage of"+ str(min)+" to" +str(max))
 
 def convert(val):
     return float(format(val*9.0/5.0 + 32, '.2f'))
+
+def printF(f):
+    for k in f:
+        print(str(k) +"C is " + str(f[k]) + " F")
+        
 
 validate(-51,106,"correct value")
 
